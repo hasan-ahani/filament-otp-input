9f90c75cfb7bc4110379c2d57d248e7e43760255
Hasan Ahani <hasanahani.ir@gmail.com>
2024-05-09 12:58:31 +0330

    [ADD] Auto complete

--- resources/views/components/otp-input.blade.php
+++ resources/views/components/otp-input.blade.php
@@ -15,6 +15,7 @@
     $numberInput = $getNumberInput();
     $isAutofocused = $isAutofocused();
     $inputType = $getType();
+    $autocomplete = $getAutocomplete();
 @endphp
 
 <x-dynamic-component
@@ -80,7 +81,7 @@
             },
         }">
         <div class="flex justify-between gap-6 fi-otp-input-container">
- 
+
             @foreach(range(1, $numberInput) as $column)
 
                 <x-filament::input.wrapper
@@ -107,6 +108,7 @@
                         maxlength="1"
                         x-ref="{{$column}}"
                         required
+                        autocomplete="{{$autocomplete}}"
                         class="fi-input fi-otp-input block w-full border-none py-1.5 text-base text-gray-950 transition duration-75 placeholder:text-gray-400 focus:ring-0 disabled:text-gray-500 disabled:[-webkit-text-fill-color:theme(colors.gray.500)] disabled:placeholder:[-webkit-text-fill-color:theme(colors.gray.400)] dark:text-white dark:placeholder:text-gray-500 dark:disabled:text-gray-400 dark:disabled:[-webkit-text-fill-color:theme(colors.gray.400)] dark:disabled:placeholder:[-webkit-text-fill-color:theme(colors.gray.500)] sm:text-sm sm:leading-6 bg-white/0 ps-3 pe-3 text-center"
                         x-on:input="handleInput($event, {{$column}})"
                         x-on:paste="handlePaste($event)"
